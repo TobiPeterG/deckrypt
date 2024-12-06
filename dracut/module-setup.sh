6d37a51ff2de076ce9ba5aa525da108c8e1edf7c
TobiPeterG <github.threefold020@passmail.net>
2024-12-06 11:15:33 +0100

    Add configs from /usr/share/deckrypt

--- dracut/module-setup.sh
+++ dracut/module-setup.sh
@@ -18,6 +18,12 @@ install() {
 
     inst_simple "$moddir/deckrypt.service" "$systemdsystemunitdir/deckrypt.service"
     $SYSTEMCTL -q --root "$initdir" enable deckrypt.service
-    inst /etc/deckrypt /etc/deckrypt
-    inst_multiple $(find /etc/deckrypt -type f)
+    if [ -d "/etc/deckrypt" ]; then
+        inst /etc/deckrypt /etc/deckrypt
+        inst_multiple $(find /etc/deckrypt -type f)
+    fi
+    if [ -d "/usr/share/deckrypt" ]; then
+        inst /usr/share/deckrypt /usr/share/deckrypt
+        inst_multiple $(find /usr/share/deckrypt -type f)
+    fi
 }
